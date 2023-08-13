831bc201a08b5f78efd9595765075351be53dd59
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-08-13 08:08:23 -0700

    bnbclone

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,9 +4,7 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-import console
 from console import HBNBCommand
-
 
 class TestConsole(unittest.TestCase):
     """Class for testing the console"""
@@ -24,14 +22,14 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertTrue(self.console.onecmd("quit"))
             output = f.getvalue().strip()
-            self.assertEqual(output, "")  # Check if output is empty
+            self.assertEqual(output, "")
 
     def test_EOF_command(self):
         """Test the EOF command"""
         with patch('sys.stdout', new=StringIO()) as f:
             self.assertTrue(self.console.onecmd("EOF"))
             output = f.getvalue().strip()
-            self.assertEqual(output, "")  # Check if output is empty
+            self.assertEqual(output, "")
 
     def test_help_show_command(self):
         """Test the help command for 'show'"""
@@ -42,8 +40,30 @@ class TestConsole(unittest.TestCase):
             self.assertIn("Usage: show <class name> <id>", output)
             # Add more assertions based on expected help text
 
-    # Add more test methods to cover all commands and features of the console
-    # Use the recommended patching technique to intercept STDOUT
+    def test_create_command(self):
+        """Test the create command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) > 0)  # Check if output is not empty
+
+    def test_show_command_missing_class(self):
+        """Test the show command with missing class name"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("show")
+            output = f.getvalue().strip()
+            self.assertIn("** class name missing **", output)
+
+    # Continue adding more test methods for other commands and features
+
+    def test_create_invalid_class(self):
+        """Test create command with invalid class name"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create InvalidClass")
+            output = f.getvalue().strip()
+            self.assertIn("** class doesn't exist **", output)
+
+    # Add more test methods for other commands and features
 
 if __name__ == '__main__':
     unittest.main()
