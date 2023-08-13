5043c24e230ebf4865bc1703bf1b33f9c42415c0
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-13 19:29:46 +0100

    modify console test file description

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for console.py.
-
 Unittest classes:
     TestHBNBCommand_prompting
     TestHBNBCommand_help
@@ -11,6 +10,7 @@ Unittest classes:
     TestHBNBCommand_destroy
     TestHBNBCommand_update
 """
+
 import os
 import sys
 import unittest
