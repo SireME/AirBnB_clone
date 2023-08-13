e20be91d692fefe24dc47b453d64b3aace30d566
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-08-12 18:23:18 -0700

    bnbclone

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""Test for the console"""
 
 import unittest
-import console
+from unittest.mock import patch
+from io import StringIO
 from console import HBNBCommand
 
 
 class TestConsole(unittest.TestCase):
-    """Class for testing the console"""
+    def setUp(self):
+        self.console = HBNBCommand()
 
-    def create_instance(self):
-        """Create an instance of HBNBCommand"""
-        return HBNBCommand()
+    def tearDown(self):
+        pass
 
-    def test_quit_command(self):
-        """Test the quit command"""
-        console_instance = self.create_instance()
-        self.assertTrue(console_instance.onecmd("quit"))
+    def test_quit(self):
+        with self.assertRaises(SystemExit):
+            self.console.onecmd("quit")
 
-    def test_eof_command(self):
-        """Test the EOF command"""
-        console_instance = self.create_instance()
-        self.assertTrue(console_instance.onecmd("EOF"))
+    def test_help(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("help")
+            self.assertIn("Documented commands (type help <topic>):", f.getvalue())
+
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create BaseModel")
+            self.assertIn("BaseModel", f.getvalue())
+
+    def test_show(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+            
+            f.clear()
+            self.console.onecmd("show BaseModel " + obj_id)
+            self.assertIn(obj_id, f.getvalue())
+
+if __name__ == '__main__':
+    unittest.main()
 
